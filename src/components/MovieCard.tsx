e4efb7ced143e3829498a46564ca9f33b3b356ad
grisley <jorge.cobreros@protonmail.com>
2024-09-06 12:55:59 +0200

    Card and image styling

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,20 +1,22 @@
 import { Movie } from '../types'
 import placeholder from'../assets/no-image.jpg'
-import { Image } from '@nextui-org/react'
 
 const MovieCard = ({movie}: {movie: Movie}) => {
   const { title, year, poster } = movie
   const posterImage = poster || placeholder
   return (
     <div className='movie-item-container'>
-      <Image 
-        className='movie-image'
-        src={posterImage}
-        alt={title}
-      />
-      <p className='movie-title'>
-        {title} - {year}
-      </p>
+      <div className='movie-image-wrapper'>
+        <img 
+          className='movie-image'
+          src={posterImage}
+          alt={title}
+        />
+      </div>
+      <div className='movie-title'>
+        <h2>{title} </h2> 
+        <h3>{year} </h3> 
+      </div>
     </div>
     
   )

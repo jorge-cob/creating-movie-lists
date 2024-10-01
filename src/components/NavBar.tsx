d045a60db8a19b8c699ba137013b01d9eeef5c02
grisley <jorge.cobreros@protonmail.com>
2024-10-01 19:10:32 +0200

    styling navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,10 +3,17 @@ import SearchBar from "./SearchBar";
 
 const NavBar = () => {
   return (
-    <div className="navbar">
-      <SearchBar />
-      <Link to={'/mylists'}>My lists</Link>
-    </div>
+    <nav className="bg-blue-500 border-gray-200 dark:bg-gray-900">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-center mx-auto p-4 space-x-8">
+        <SearchBar />
+        <Link 
+          to={'/mylists'}
+          className="text-white font-bold hover:text-gray-700 hover:underline"
+        >
+          My lists
+        </Link>
+      </div>
+    </nav>
   )
 }
 

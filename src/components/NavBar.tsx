86a10ccb197e32190fe5b9ed4ba417e0f2494555
grisley <jorge.cobreros@protonmail.com>
2024-10-07 12:38:27 +0200

    Navbar styles

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,19 +1,28 @@
 import { Link } from "react-router-dom";
 import SearchBar from "./SearchBar";
+import { NavbarContent, Navbar, NavbarBrand, Image, NavbarItem } from "@nextui-org/react";
 
 const NavBar = () => {
   return (
-    <nav className="bg-blue-500 border-gray-200 dark:bg-gray-900">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-center mx-auto p-4 space-x-8">
-        <SearchBar />
-        <Link 
-          to={'/mylists'}
-          className="text-white font-bold hover:text-gray-700 hover:underline"
-        >
-          My lists
-        </Link>
-      </div>
-    </nav>
+    <Navbar className="bg-blue-500 border-gray-200 dark:bg-gray-900">
+        <NavbarBrand>
+          <Image width="30px" height="30px"   src="../../logo.svg"  alt="fireSpot"/>
+          <p className="font-bold text-inherit pl-10">MisPelis</p>
+        </NavbarBrand>
+        <NavbarContent className="hidden sm:flex gap-4" justify="center">
+          <NavbarItem>
+            <SearchBar />
+          </NavbarItem>
+          <NavbarItem>
+            <Link 
+              to={'/mylists'}
+              className="text-white font-bold hover:text-gray-700 hover:underline"
+            >
+              My lists
+            </Link>
+          </NavbarItem>
+        </NavbarContent>
+    </Navbar>
   )
 }
 
